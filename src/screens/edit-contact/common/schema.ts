9f50410420f0568c12451e9e26d06913eb946f36
renomi <renomi19@gmail.com>
2023-07-13 17:54:17 +0700

    feat(ui): edit contact components

---
+++ src/screens/edit-contact/common/schema.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+export const editContactValidation = z.object({
+  firstName: z.string().min(1, 'make sure first name is valid'),
+  lastName: z.string().min(1, 'make sure last name is valid'),
+  age: z.coerce.number().gt(0, 'make sure age is valid').or(z.string()),
+  photo: z
+    .string()
+    .url('photo must be valid url')
+    .min(1, 'photo must be valid url'),
+});
+
+export type EditContactSchema = z.infer<typeof editContactValidation>;
--- src/screens/edit-contact/index.tsx
+++ src/screens/edit-contact/index.tsx
@@ -1,18 +1,80 @@
+import { useCallback, useRef } from 'react';
+import type { RootState } from '@/redux/store';
 import { useAppSelector } from '@/hooks';
 import { ScrollView, StyleSheet } from 'react-native';
-import { Text } from 'react-native-paper';
-import type { RootState } from '@/redux/store';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import {
+  EditContactSchema,
+  editContactValidation,
+} from '@/screens/edit-contact/common/schema';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import { BottomSheetModal } from '@gorhom/bottom-sheet';
+import { Input, Button } from '@/ui';
+import { ConfirmationSuccess } from '@/ui/modal/confirmation-success';
 
 const selectContact = (state: RootState) => state.userState.contact;
 
 export const EditContactScreen = () => {
   const currentContact = useAppSelector(selectContact);
+  const sheetModalSuccess = useRef<BottomSheetModal>(null);
+
+  const {
+    control,
+    formState: { errors, isDirty },
+    handleSubmit,
+  } = useForm<EditContactSchema>({
+    reValidateMode: 'onChange',
+    resolver: zodResolver(editContactValidation),
+    defaultValues: {
+      ...currentContact,
+      //@ts-expect-error
+      age: currentContact.age.toString(),
+    },
+  });
+
+  const onSubmit: SubmitHandler<EditContactSchema> = useCallback(
+    updatedContact => {
+      console.log('🧐 ~ onSubmit: ~ updatedContact:', updatedContact);
+    },
+    [],
+  );
+  // console.log('🧐 ~ EditContactScreen ~ errors:', errors);
   console.log('🧐 ~ EditContactScreen ~ currentContact:', currentContact);
+
   return (
     <ScrollView contentContainerStyle={styles.screen}>
-      <Text variant="bodyMedium" style={styles.info}>
-        Edit Contact Screen
-      </Text>
+      <Input
+        control={control}
+        name="firstName"
+        label="first name"
+        placeholder="first name"
+      />
+      <Input
+        control={control}
+        name="lastName"
+        label="last name"
+        placeholder="last name"
+      />
+      <Input control={control} name="age" label="age" placeholder="age" />
+      <Input
+        control={control}
+        name="photo"
+        label="photo"
+        placeholder="input image url"
+      />
+
+      <Button
+        disabled={!isDirty}
+        onPress={handleSubmit(onSubmit)}
+        style={styles.btn}>
+        Update Contact
+      </Button>
+
+      <ConfirmationSuccess
+        message="Contact has been edited succsessfully"
+        ref={sheetModalSuccess}
+      />
     </ScrollView>
   );
 };
@@ -20,11 +82,11 @@ export const EditContactScreen = () => {
 const styles = StyleSheet.create({
   screen: {
     flexGrow: 1,
-    justifyContent: 'center',
-    padding: 16,
+    rowGap: 8,
+    paddingHorizontal: 24,
+    paddingTop: 32,
   },
-  info: {
-    maxWidth: 400,
-    textAlign: 'center',
+  btn: {
+    marginTop: 32,
   },
 });
